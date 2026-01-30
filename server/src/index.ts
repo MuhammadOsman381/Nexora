2273dfb52cbc7028028afa53acd8a85557d60a5e
Muhammad Osman <osman@Muhammads-MacBook-Air.local>
2026-01-30 14:59:36 +0500

    tyrin to fix cors on server

--- server/src/index.ts
+++ server/src/index.ts
@@ -14,6 +14,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   'http://localhost:5173',
+  "https://nexora-seven-rosy.vercel.app/"
 ];
 
 app.use(cors({ origin: allowedOrigins }));
