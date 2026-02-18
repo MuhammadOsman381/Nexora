6cdec9b2c5a312f89c8cc062b9f1922b5b0c2e86
Muhammad Osman <mosman257@gmail.com>
2026-02-18 23:33:35 +0500

    --

--- server/src/index.ts
+++ server/src/index.ts
@@ -44,7 +44,7 @@ app.get('/', (_req, res) => {
   res.send('Hello from server!');
 });
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
